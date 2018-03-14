1976c69a6767b7533b091a84f13b0357c8981101
Jacklau9515 <Jacklau9515@gmail.com>
2018-03-14 23:46:14 +1100

    Initial commit

---
+++ Controllers/Deletepart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApi.Controllers
+{
+    [Route("api/arraycalc/[controller]")]
+    public class DeletePart : Controller
+    {
+        [HttpGet]
+        public string Get(int position, int[] productIds)
+        {
+            string outString = "[";
+            int count = 0;
+            for (int i = 0; i < productIds.Length; i++)
+            {
+                if (productIds[i] != position)
+                {
+                    if (count != 0) outString += ","; 
+                    outString += productIds[i];
+                    count++;
+                }
+            }
+            outString += "]";
+            return outString;
+        }
+
+        static bool Delete(int deleteWhat, int compare)
+        {
+            return compare != deleteWhat;
+        }
+    }
+}
---
+++ Controllers/Reverse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApi.Controllers
+{
+    [Route("api/arraycalc/[controller]")]
+    public class Reverse : Controller
+    {
+        [HttpGet]
+        public string Get(int[] productIds)
+        {
+            string outString = "[";
+            for(int i=0; i<productIds.Length/2; i++)
+            {
+                int tmp = productIds[i];
+                productIds[i] = productIds[productIds.Length - i - 1];
+                productIds[productIds.Length - i - 1] = tmp;
+            }
+            for(int i=0; i<productIds.Length-1; i++) outString += (productIds[i]+",");
+            outString += (productIds[productIds.Length-1]+"]");
+            return outString;
+        }
+    }
+}
